a48e68815f5e57b669e1604385532277e75c1617
KARR Matthieu <matthieukarr@hotmail.fr>
2025-03-18 00:37:09 +0100

    ajustement berthopics

--- st_app/streamlit_bank.py
+++ st_app/streamlit_bank.py
@@ -968,7 +968,8 @@ st.write(df['topic'].value_counts())
                 st.code(code_lda, language="python")
 
             st.image("st_app/LDA10thm.png")
-            st.image("st_app/bertopics_graph8.png")
+            st.write(Graphique de gensimvis pour les 8 thèmes les plus importants)
+            st.image("st_app/lda_graph8.png")
 
         if st.checkbox("Méthode Bertopics", key="bertopics1"):
             import pandas as pd
