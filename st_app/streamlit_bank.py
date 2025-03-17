28dd7f9f50d8f5c6fb95f99da3f3ac1076229df0
KARR Matthieu <matthieukarr@hotmail.fr>
2025-03-17 23:22:17 +0100

    ajustement texte modelisation

--- st_app/streamlit_bank.py
+++ st_app/streamlit_bank.py
@@ -716,7 +716,8 @@ de variables indépendantes."""
         st.write(
             st.markdown(r"""
                         
-            Après avoir équilibré les données, il est essentiel de normaliser leurs caractéristiques pour éviter tout biais lié aux écarts d'échelle. Le **MaxAbsScaler** est particulièrement adapté aux données textuelles vectorisées (comme TF-IDF), car :
+            Après avoir équilibré les données, il est essentiel de normaliser leurs caractéristiques pour éviter tout biais
+            lié aux écarts d'échelle. Le **MaxAbsScaler** est particulièrement adapté aux données textuelles vectorisées car :
             - Il **conserve la sparsité** des matrices, optimisant ainsi les performances.
             - Il **met à l'échelle proportionnellement** dans l'intervalle [-1, 1], préservant les proportions initiales.
 
